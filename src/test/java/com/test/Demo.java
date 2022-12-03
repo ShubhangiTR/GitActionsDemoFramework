5d4acc04dbfd31561beb7897ed507a7f5e34450e
Singhal <C288758@ead.thomsonreuters.com>
2022-12-03 21:23:12 +0530

    Initial commit

---
+++ src/test/java/com/test/Demo.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+    public WebDriver driver;
+    public String baseUrl = "https://www.lambdatest.com/";
+
+    @Test
+    public void test1() {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(baseUrl);
+        driver.close();
+    }
+}
